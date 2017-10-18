f7cdda9ec2117f2018963520d14fa958d365e907
jack8n <alexhotianovich@gmail.com>
2017-10-18 14:41:27 +0300

    Update email login and password

--- EventPlanning/Scripts/SiteScripts.js
+++ EventPlanning/Scripts/SiteScripts.js
@@ -11,25 +11,6 @@ $(function () {
     });
 });
 
-/*=================Modal Window AddEmployee Insert ==================================================*/
-function checkEmail(){
-    var email = $('#emailLogin').val()
-
-    if (email.length != 0) {
-        var pattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.([a-z]{1,6}\.)?[a-z]{2,6}$/i;
-        if (email.search(pattern) == 0) {
-            $('#send').removeAttr('disabled');
-            $('#valid').text('');
-        } else {
-            $('#valid').css('color', 'red');
-            $('#valid').text('Введите email');
-            $('#send').attr('disabled', 'disabled');
-        }
-
-    } else {
-        $('#send').attr('disabled', 'disabled');
-    }
-}
 /*==================================Clear form ========================*/
 
 function ResetView() {
